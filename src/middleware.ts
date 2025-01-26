ea145499f76e6128ff90269309a9e4f03213c08f
Satyam <2021pcecysatyam050@poornima.org>
2025-01-26 10:22:55 +0530

    fix: fix the auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,52 +1,30 @@
-import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
-import { getToken } from "next-auth/jwt"
-
-
-// export async function middleware(request: NextRequest) {
-//   // Check if the request is for a static asset
-//   const isStaticAsset =
-//     request.nextUrl.pathname.startsWith("/_next") ||
-//     request.nextUrl.pathname.match(/\.(svg|png|jpg|jpeg|gif|ico)$/);
-
-//   // If it's a static asset, allow the request to proceed
-//   if (isStaticAsset) {
-//     return NextResponse.next();
-//   }
-//   const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })
-//   console.log("token", token)
-
-//   if (!token && !request.nextUrl.pathname.startsWith("/auth")) {
-//     return NextResponse.redirect(new URL("/auth", request.url))
-//   }
-//   return NextResponse.next()
-// }
-
-// export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-// }
-
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })
-  const { pathname } = request.nextUrl
+  // Check if the request is for a static asset
+  const isStaticAsset =
+    request.nextUrl.pathname.startsWith("/_next") ||
+    request.nextUrl.pathname.match(/\.(svg|png|jpg|jpeg|gif|ico)$/);
 
-  // Allow access to authentication routes
-  if (pathname.startsWith("/api/auth") || pathname === "/auth") {
-    return NextResponse.next()
+  if (isStaticAsset) {
+    return NextResponse.next();
   }
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  console.log("token", token);
 
-  // Redirect to /auth if accessing a protected route without a token
-  if (!token && pathname !== "/auth") {
-    return NextResponse.redirect(new URL("/auth", request.url))
+  if (!token && !request.nextUrl.pathname.startsWith("/auth")) {
+    return NextResponse.redirect(new URL("/auth", request.url));
+  } else if (token && request.nextUrl.pathname.startsWith("/auth")) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
-
-  // Allow access if authenticated
-  return NextResponse.next()
+  return NextResponse.next();
 }
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
-
-
+};
